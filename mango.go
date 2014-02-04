86519c6f78774502da04cc3081b07fd87e30f065
slyrz <slyrz@users.noreply.github.com>
2014-02-04 17:47:48 +0100

    truncate existing files

--- mango.go
+++ mango.go
@@ -98,7 +98,7 @@ func (b *Builder) feedOptions() {
 }
 
 func (b *Builder) Save(path string) error {
-	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		return err
 	}
