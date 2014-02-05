b6ee9a980d13fa1b9d4aaeb95be3373240cffb16
slyrz <slyrz@users.noreply.github.com>
2014-02-05 17:24:40 +0100

    more sections, more comments

--- markup/writer.go
+++ markup/writer.go
@@ -6,6 +6,23 @@ import (
 	"time"
 )
 
+var (
+	// The array below contains a few conventional or suggested sections, that
+	// should be placed at the start of the manual page in the order shown in
+	// the list. We check if the documentation contains some of these sections
+	// and print the ones we found first.
+	sections = []string{
+		"name",
+		"synopsis",
+		"configuration",
+		"description",
+		"options",
+		"exit status",
+		"return value",
+		"errors",
+	}
+)
+
 type Writer interface {
 	WriteTitle(name string)
 	WriteDate(date time.Time)
@@ -20,36 +37,30 @@ type Writer interface {
 }
 
 func Save(writer Writer, out io.Writer) error {
-	// Special sections we care about. We want to print them in this order,
-	// no matter where they were defined.
-	sections := []string{
-		"name",
-		"synopsis",
-		"description",
-		"options",
-	}
-
 	bufOut := bufio.NewWriter(out)
 	defer bufOut.Flush()
 
 	head := writer.Head()
 	tail := writer.Tail()
 
+	// Head contains stuff the writer needs to put at the document start.
 	if _, err := bufOut.WriteString(head); err != nil {
 		return err
 	}
+	// Print the conventional manual page sections.
 	parts := writer.Parts()
 	for _, name := range sections {
 		if data, ok := parts[name]; ok {
 			if _, err := bufOut.WriteString(data); err != nil {
 				return err
 			}
-			// Delete from map so we don't print it again in the second pass.
+			// Delete section from map so we don't print it again
+			// in the second pass.
 			delete(parts, name)
 		}
 	}
-
-	// Print the remaining parts in the original order.
+	// After we printed the conventional sections, we print the user defined
+	// sections now and we keep their order.
 	for _, name := range writer.Order() {
 		if data, ok := parts[name]; ok {
 			if _, err := bufOut.WriteString(data); err != nil {
@@ -57,10 +68,9 @@ func Save(writer Writer, out io.Writer) error {
 			}
 		}
 	}
-
+	// Tail contains stuff the writer needs to put at the document end.
 	if _, err := bufOut.WriteString(tail); err != nil {
 		return err
 	}
-
 	return nil
 }
