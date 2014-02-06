a0291859e60135e651305c2f9e781ac53fe764c5
slyrz <slyrz@users.noreply.github.com>
2014-02-06 16:01:36 +0100

    changed comment text

--- example/example.go
+++ example/example.go
@@ -1,10 +1,11 @@
-// example_command - shows the basic usage of mango
+// example - shows the basic usage of mango
 //
 // Description:
 //
-// It doesn't take much to create manual pages with mango. Just put a comment
-// at the top of your source code file and write down stuff you want to include
-// in the manual page like this. Feel free to add as many sections as you want.
+// It doesn't take much to create manual pages with mango. Just write down
+// stuff you want to include in the manual page in a comment at the top
+// of your source file, like this. Feel free to add as many sections as you
+// want.
 package main
 
 import (
@@ -14,17 +15,18 @@ import (
 var (
 	optFoo = flag.Bool("foo", false, "this text should show up in the manual page")
 
-	// If the flag definition follows a comment like this, mango will use
-	// the text in the comment as description in the manual page.
+	// If the flag definition follows a comment like this, mango uses the
+	// comment as description in the manual page.
 	optBar = flag.Bool("bar", false, "the above comment should show up in the manual page")
-	optBaz = false
+	optBaz = ""
 )
 
 func init() {
-	// These two calls reference the same variable and should appear
-	// grouped in the manual page.
-	flag.BoolVar(&optBaz, "baz", false, "two calls, but one entry in the manual")
-	flag.BoolVar(&optBaz, "b", false, "two calls, but one entry in the manual")
+	// These two calls reference the same variable and will appear
+	// grouped in the manual page. Since these aren't boolean flags, mango
+	// prints the argument type as well.
+	flag.StringVar(&optBaz, "baz", "", "two calls, but one entry in the manual")
+	flag.StringVar(&optBaz, "b", "", "two calls, but one entry in the manual")
 }
 
 func main() {
