e5fcfc74a107b9a93eff03e5408f1ecb38215d53
slyrz <slyrz@users.noreply.github.com>
2015-07-12 00:39:22 +0200

    preview output in man

--- mango.go
+++ mango.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	_ "fmt"
 	"os"
+	"os/exec"
 )
 
 var options = struct {
-	Output string
-	Name   string
-	Plain  bool
+	Output  string
+	Name    string
+	Plain   bool
+	Preview bool
 }{}
 
 func init() {
 	flag.StringVar(&options.Output, "output", "", "write output to file")
-	flag.StringVar(&options.Name, "name", "", "command name")
-	flag.BoolVar(&options.Plain, "plain", false, "plain text comments")
+	flag.StringVar(&options.Name, "name", "", "set command name")
+	flag.BoolVar(&options.Plain, "plain", false, "treat comments as plain text")
+	flag.BoolVar(&options.Preview, "preview", false, "preview output in man")
 }
 
 func getReader() Reader {
@@ -39,20 +42,35 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-
 		text, err := builder.Build(file)
 		if err != nil {
 			panic(err)
 		}
-		if options.Output == "" {
-			fmt.Println(text)
-		} else {
-			dst, err := os.Create(options.Output)
+		if options.Preview {
+			cmd := exec.Command("groff", "-Wall", "-mtty-char", "-mandoc", "-Tascii")
+			cmd.Stdout = os.Stdout
+			cmd.Stderr = os.Stderr
+			inp, err := cmd.StdinPipe()
 			if err != nil {
 				panic(err)
 			}
-			dst.Write([]byte(text))
-			dst.Close()
+			if err := cmd.Start(); err != nil {
+				panic(err)
+			}
+			inp.Write([]byte(text))
+			inp.Close()
+			cmd.Wait()
+		} else {
+			if options.Output == "" {
+				os.Stdout.Write([]byte(text))
+			} else {
+				dst, err := os.Create(options.Output)
+				if err != nil {
+					panic(err)
+				}
+				dst.Write([]byte(text))
+				dst.Close()
+			}
 		}
 	}
 }
